5d1500fe0a1ac40f76c31c84fd69b5894982f66b
Danilo Hoffmann <dhhyi@aol.com>
2019-09-21 22:28:58 +0200

    build minimal data view example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { merge, Observable, of, timer } from 'rxjs';
+import { mapTo, switchMap } from 'rxjs/operators';
+import { authorDemoData, upsertAuthor } from './store/author';
+import { bookDemoData, upsertBook } from './store/book';
+import { BookView, getBookView } from './store/book-view';
+import { tagDemoData, upsertTag } from './store/tag';
 
 @Component({
   selector: 'ngx-app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx-data-views';
+
+  books: { book$: Observable<BookView>; loading$: Observable<boolean> }[];
+
+  constructor(private readonly store: Store<{}>) {}
+
+  ngOnInit(): void {
+    this.books = bookDemoData.map(book => {
+      // stream for book selected from store
+      const book$ = this.store.pipe(select(getBookView, book.id));
+
+      return {
+        book$,
+        // display loading when book was updated
+        loading$: book$.pipe(
+          switchMap(() => merge(of(true), timer(2000).pipe(mapTo(false))))
+        )
+      };
+    });
+  }
+
+  updateAuthor(authorId: string): void {
+    this.store.dispatch(
+      upsertAuthor({ author: authorDemoData.find(a => a.id === authorId) })
+    );
+  }
+
+  updateBook(bookId: string): void {
+    this.store.dispatch(
+      upsertBook({ book: bookDemoData.find(a => a.id === bookId) })
+    );
+  }
+
+  updateTag(tagId: string): void {
+    this.store.dispatch(
+      upsertTag({ tag: tagDemoData.find(a => a.id === tagId) })
+    );
+  }
 }
---
+++ src/app/store/book-view/index.ts
@@ -0,0 +1,26 @@
+import { createSelector } from '@ngrx/store';
+import { Author } from '../author';
+import { getAuthorsOfBook, getBook, getTagsOfBook } from '../book';
+import { Tag } from '../tag';
+
+export interface BookView {
+  title: string;
+  description: string;
+  authors: Author[];
+  tags: Tag[];
+  published: number;
+}
+
+export const getBookView = createSelector(
+  getBook,
+  getAuthorsOfBook,
+  getTagsOfBook,
+  (book, authors, tags): BookView =>
+    book && {
+      title: book.title,
+      description: book.description,
+      published: book.published,
+      authors,
+      tags
+    }
+);
